a88ccfe7e058b7c7831406fb804176ae50dd91c7
ameyo <anshulrawat@drishti-soft.com>
2021-05-09 14:06:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MyNavbar from "./components/MyNavbar";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <MyNavbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
