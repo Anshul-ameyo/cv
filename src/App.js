18899e321d6eda88db9afdd526dfe00a24e27ee7
ameyo <anshulrawat@drishti-soft.com>
2021-05-09 15:33:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import MyCarousal from "./components/MyCarousal";
 import MyNavbar from "./components/MyNavbar";
+import TitleMessage from "./components/TitleMessage";
 
 const App = () => {
   return (
     <>
       <div>
         <MyNavbar />
+        <MyCarousal />
+        <TitleMessage />
       </div>
     </>
   );
